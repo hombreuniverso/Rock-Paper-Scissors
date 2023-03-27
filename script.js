365666be46f366402093eea5922b58dc1de97497
hombreuniverso <67452254+hombreuniverso@users.noreply.github.com>
2023-03-26 22:46:12 -0400

    Complete javascript game logic
    playerChoice, ComputerChoice, Comparison

--- script.js
+++ script.js
@@ -1,63 +1,120 @@
-const playerInput = document.querySelector('input');
+const playerInput = document.querySelector("select");
 
-const pTag = document.querySelector('p');
+const playerOutput = document.querySelector("#playerOutput");
+const computerOutput = document.querySelector("#computerOutput");
+const results = document.querySelector("#results");
 
-playerInput.addEventListener('change', () => { 
-    
-//Generate computerChoice
+playerInput.addEventListener("change", () => {
+
+  //Get player choice
+  let playerOption = playerInput.value;
+
+  //Generate random number between 0 and 2
+  let computerNum = Math.floor(Math.random() * 3);
+
+  //Generate computerChoice function call
   computerChoice();
 
-//Get player choice
-  playerChoice();
-
-//Compare and choose winner or draw
-  comparePlayers();
-  
-  console.log(playerChoice());
-    
-}) 
-
 //Function: Get computer Choice
-function computerChoice(){
-    //Generate random number between 0 and 2   
-    let computerNum = Math.floor(Math.random() * 3);
-    
-  /*
-    //Link each digit to a choice e.g. 0 is "rock" using if/else if/else
-    if(computerNum === 0){
-        return 'rock';
-    }else if(computerNum === 1){
-        return 'paper';
-    }else{
-        return 'scissors';
-    }
- */
-//Link each digit to a choice e.g. 0 is "rock" using switch
-    switch(true){
-        case computerNum === 0:
-            return 'rock';
-            break;
-        case computerNum === 1:
-            return 'paper';
-            break;
-        case computerNum === 2: 
-            return 'scissors';
-            break;
-    }
+function computerChoice() {  
+
+  //Link each digit to a choice e.g. 0 is "rock" using if/else if/else
+  if (computerNum === 0) {
+    return "rock";
+  } else if (computerNum === 1) {
+    return "paper";
+  } else if(computerNum === 2){
+    return "scissors";
+  }
+
+  /*    
+  //Link each digit to a choice e.g. 0 is "rock" using switch
+  switch (true) {
+    case computerNum === 0:
+      return "rock";
+      break;
+    case computerNum === 1:
+      return "paper";
+      break;
+    case computerNum === 2:
+      return "scissors";
+      break;
+  }
+*/
 }
 
-//Function: Get player choice 
-function playerChoice(){
-    let playerChoiceLc = playerInput.value.toLowerCase();
-    //Check if player gives any of the three legitimate choices
-    if(playerChoiceLc !== 'rock' && playerChoiceLc !== 'paper' && playerChoiceLc !== 'scissors'){
-        alert("Choose either rock");
-    }else{
-        return playerChoiceLc;
-    }
-}
+  //Compare players (win/ draw) function call
+  comparePlayers();
 
 //Function: Compare players (win/ draw)
-function comparePlayers(){
-    
+function comparePlayers() {
+  if (playerOption === "rock" && computerChoice() === "paper") {
+    playerOutput.textContent =
+      "Player's Choice: " +
+      playerOption
+    computerOutput.textContent =
+      "Computer's choice: " +
+      computerChoice() 
+    results.textContent =
+      "Computer wins!";
+  } else if (playerOption === "paper" && computerChoice() === "rock") {
+    playerOutput.textContent =
+      "Player's Choice: " +
+      playerOption
+    computerOutput.textContent =
+      "Computer's choice: " +
+      computerChoice() 
+    results.textContent =
+      "Congratulations! You win!";
+  } else if (playerOption === "rock" && computerChoice() === "scissors") {
+    playerOutput.textContent =
+      "Player's Choice: " +
+      playerOption
+    computerOutput.textContent =
+      "Computer's choice: " +
+      computerChoice() 
+    results.textContent =
+      "Congratulations! You win!";
+  } else if (playerOption === "scissors" && computerChoice() === "rock") {
+    playerOutput.textContent =
+    "Player's Choice: " +
+    playerOption
+  computerOutput.textContent =
+    "Computer's choice: " +
+    computerChoice() 
+  results.textContent =
+      "Computer wins!";
+  } else if (playerOption === "paper" && computerChoice() === "scissors") {
+    playerOutput.textContent =
+    "Player's Choice: " +
+    playerOption
+  computerOutput.textContent =
+    "Computer's choice: " +
+    computerChoice() 
+  results.textContent =
+      "Computer wins!";
+  } else if (playerOption === "scissors" && computerChoice() === "paper") {
+    playerOutput.textContent =
+      "Player's Choice: " +
+      playerOption
+    computerOutput.textContent =
+      "Computer's choice: " +
+      computerChoice() 
+    results.textContent =
+      "Congratulations! You win!";
+  } else if (playerOption === computerChoice()) {
+    playerOutput.textContent =
+      "Player's Choice: " +
+      playerOption
+    computerOutput.textContent =
+      "Computer's choice: " +
+      computerChoice() 
+    results.textContent =
+      "A draw";
+  } 
 }
+
+});
+
+
+
